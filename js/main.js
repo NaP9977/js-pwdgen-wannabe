7aad5f337feb6a58354c09eb2b4cbd78bb14b474
NaP <annposillipo@gmail.com>
2023-06-29 16:17:18 +0200

    bonus1

--- js/main.js
+++ js/main.js
@@ -2,4 +2,16 @@ let name = prompt("Qual è il tuo nome?");
 let surname = prompt("Qual è il tuo cognome?");
 let favouriteColor = prompt("Qual è il tu colore preferito?");
  let password = name + surname + favouriteColor + "21"
- console.log(password);
+ 
+ let number1 = prompt("Dimmi un numero");
+ let number2 = prompt("Dimmi un altro numero")
+
+ let divisor = number1/number2;
+
+ let password2 = name + surname + favouriteColor + divisor;
+ 
+ 
+console.log(password);
+console.log(password2);
+
+
