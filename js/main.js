b047646ebe2222f66f51e12473510e4a8468d25e
NaP <annposillipo@gmail.com>
2023-06-29 16:05:23 +0200

    Main_part_completed

---
+++ js/main.js
@@ -0,0 +1,5 @@
+let name = prompt("Qual è il tuo nome?");
+let surname = prompt("Qual è il tuo cognome?");
+let favouriteColor = prompt("Qual è il tu colore preferito?");
+ let password = name + surname + favouriteColor + "21"
+ console.log(password);
